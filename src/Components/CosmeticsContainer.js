499d95226ec3daba71965117aff8564dcbaf1de8
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-05 08:03:43 -0700

    added rows updated features

--- src/Components/CosmeticsContainer.js
+++ src/Components/CosmeticsContainer.js
@@ -2,17 +2,58 @@ import React from "react";
 import CosmeticsCard from "./CosmeticsCard";
 import MoreButton from "./MoreButton";
 
-function CosmeticsContainer({luxuryCosmetics, scrollThroughMoreItems, scrollThroughPreviousItems}) {
+function CosmeticsContainer({ luxuryCosmetics, faceList, lipsList, eyesList, scrollThroughMoreItems, scrollThroughPreviousItems}) {
 
-    const cosmeticsCards = luxuryCosmetics.map((cosmetic) => {
+    const cosmeticsCards = luxuryCosmetics.map((cosmetic) => <CosmeticsCard key={cosmetic.id} cosmetic={cosmetic}/>)
+    const faceCards = faceList.map((cosmetic) => {
         return <CosmeticsCard key={cosmetic.id} cosmetic={cosmetic}/>
     })
+    const lipsCards = lipsList.map((cosmetic) => {
+        return <CosmeticsCard key={cosmetic.id} cosmetic={cosmetic} />
+    })
 
+    const eyesCards = eyesList.map((cosmetic) => {
+        return <CosmeticsCard  key={cosmetic.id} cosmetic={cosmetic}/>
+    })
 
     return (
-        <div className="CardContainer">
+        <div>
+            <div className="CardContainer">   
+            <h2>Luxury</h2>
             {cosmeticsCards}
-            <MoreButton scrollThroughMoreItems={scrollThroughMoreItems} scrollThroughPreviousItems={scrollThroughPreviousItems}/>
+            <MoreButton 
+                scrollThroughMoreItems={scrollThroughMoreItems} 
+                scrollThroughPreviousItems={scrollThroughPreviousItems} 
+                row="luxury"
+            />
+            </div>
+            <div className="CardContainer">
+            <h2>Face</h2>
+            {faceCards}
+            <MoreButton 
+                scrollThroughMoreItems={scrollThroughMoreItems}
+                scrollThroughPreviousItems={scrollThroughPreviousItems}
+                row="face"
+            /> 
+            </div>
+            <div className="CardContainer">
+                <h2>Eyes</h2>
+                {eyesCards}
+                <MoreButton 
+                    scrollThroughMoreItems={scrollThroughMoreItems}
+                    scrollThroughPreviousItems={scrollThroughPreviousItems}
+                    row="eyes"
+                />  
+            </div>
+            <div className="CardContainer">
+            <h2>Lips</h2>
+            {lipsCards}
+                <MoreButton 
+                    scrollThroughMoreItems={scrollThroughMoreItems}
+                    scrollThroughPreviousItems={scrollThroughPreviousItems}
+                    row="lips"
+                />  
+            </div>
         </div>
     )
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({updateSearch, filterCosmetics}) {
+
+    const handleChange = (e) => {updateSearch(e.targert.value)}
     return(
         <div id="SearchBar">
-            <input type="text"/>
-            <button id="SearchButton"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX_FbatSHKsRFAvgEx26fMNQOeibgxPhwxPqtqVfJ6tA&s"/></button>
+            <input type="text"
+            placeholder="Find Your MakeUp"
+            id="search"
+            value={filterCosmetics}
+            onChange={handleChange}
+            />
+            <button id="SearchButton"><img src="https://media.discordapp.net/attachments/1147172028114272277/1148679251130847294/search.png?width=450&height=450"/>
+</button>
         </div>
     )
 }
