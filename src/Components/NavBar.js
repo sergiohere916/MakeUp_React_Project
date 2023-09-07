48d01f732e4aa3b5d1ee44491f52382a48edecaf
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-07 09:27:14 -0500

    links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,32 +1,30 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
-import { Menu } from 'semantic-ui-react'
+
 
 function NavBar({updateSearch, searchValue}) {
-
-const [active, setActive] = useState(false)
 
 
 
     return (
         <nav id='navbar'>
-            <Menu tabular>
-            <Menu.Item as='Link' to="/cosmetics/luxury" className="luxury" name='Luxury'/>
-    
-            <Menu.Item as='Link' to="/cosmetics/face" className="face"/>
-            <Menu.Item as='Link' to="/cosmetics/eyes" className="eyes">
+            <Link to="/cosmetics/luxury" className="luxury">
+                Luxury
+            </Link>
+            <Link to="/cosmetics/face" className="face">
+                Face
+            </Link>
+            <Link to="/cosmetics/eyes" className="eyes">
                 Eyes
-            </Menu.Item>
-            <Menu.Item as='Link' to="/cosmetics/lips" className="lips">
+            </Link>
+            <Link to="/cosmetics/lips" className="lips">
                 Lips
-            </Menu.Item>
-            </Menu>
+            </Link>
             <SearchBar className="search-bar"
                 updateSearch={updateSearch} 
                 searchValue={searchValue}
             />
-        
         </nav>
     )
 }
