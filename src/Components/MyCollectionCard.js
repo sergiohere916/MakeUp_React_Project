8c2cd28c94acc1d0941f7d4e1b9065bd22987ac4
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-06 23:19:12 -0700

    real time expiration added

--- src/Components/MyCollectionCard.js
+++ src/Components/MyCollectionCard.js
@@ -4,6 +4,22 @@ import {AiOutlineDelete} from 'react-icons/ai'
 function MyCollectionCard ({product, onDelete, onSubmitUpdateExpiration}){
     const {id, name, image, product_type, brand, description, expiration } = product
     const [dateInput, setDateInput] = useState("");
+    
+    const currentDate = new Date();
+
+    const inputYear = expiration.slice(0,4);
+    
+    const inputMonth = expiration.slice(5,7);
+    
+    const inputDay = expiration.slice(8,10)
+    
+    const formattedInputDate = `${inputMonth}/${inputDay}/${inputYear}`;
+ 
+    
+    const endDate = new Date(formattedInputDate);
+    
+    const daysBeforeExp = (Math.ceil((endDate - currentDate)/1000/60/60/24));
+    
 
     function handleDelete() {
         fetch(`http://localhost:6001/mycollection/${id}`, {
@@ -34,7 +50,7 @@ function MyCollectionCard ({product, onDelete, onSubmitUpdateExpiration}){
             <AiOutlineDelete className = 'delete-icon' onClick={handleDelete}/>
             <img src={image} alt={name}/>
             <h3>{name}</h3>
-            <p>Expires: {expiration} </p>
+            {daysBeforeExp ? (<p>Expiring in: {daysBeforeExp} day/s</p>) : (<p>Expiring in: </p>)}
             <form onSubmit={handleSubmit}>
                 <label>Set Expiration: </label>
                 <input type="date" value={dateInput} onChange={handleChange}/>
